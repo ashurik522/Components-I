8c421789e33f2e739b077f43397ebd60fea52405
ashurik522 <ashurik522@gmail.com>
2022-04-13 18:26:02 -0400

    menu complete

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -32,3 +32,27 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+function menuMaker(items){
+  const menu = document.createElement("div");
+  const list = document.createElement("ul");
+  
+
+  document.querySelector(".header").appendChild(menu)
+  menu.appendChild(list);
+ 
+  menu.classList.add("menu");
+  
+  
+  items.forEach(item =>{
+    const menuItem = document.createElement("li");
+    menuItem.textContent = item;
+    document.querySelector("ul").appendChild(menuItem)
+  })
+
+  document.querySelector(".menu-button").addEventListener("click", ()=>{
+    menu.classList.toggle("menu--open")
+  })
+  return menu;
+}
+
+menuMaker(menuItems)
